7284f48e941eafc17a54ae55473b853a3efd3161
ama.st <160158221@aston.ac.uk>
2026-02-02 15:57:46 +0000

    completed task 1 & 2 for lab2 haskell

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -3,9 +3,12 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10) --changed from print(MyFunc2 10)
+    print (_3rdFunc) --changed from 3rdFunc
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2 --changed from MyFunc2
+_3rdFunc = "Hello, this is 3rd Func" --changed from 3rdFunc
+
+--functions MUST start with a lowercase or underscore
+-- they can contain letters, digits, underscores and apostrphes
--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -1,4 +1,16 @@
+onePlusone = "1 + 1 = 2"
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    print (onePlusone)
+
+    --do
+    -- allows you to build with similar structure to imperative languages
+        -- MODEL ANSWER
+        --onePlusone = putStrLn("1 + 1 = 2")
+        --main = do
+            --putStrLn "Welcome to the programme. Please enter your name"
+            --name <- getLine
+            --putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+            --onePlusone-
