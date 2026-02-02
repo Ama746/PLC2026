b57f10009d936688670282cb5995313b64f84ee4
ama.st <160158221@aston.ac.uk>
2026-02-02 16:40:18 +0000

    completed task4 lab2

--- lab2-functionalPrimer/haskell/task4.hs
+++ lab2-functionalPrimer/haskell/task4.hs
@@ -1,14 +1,38 @@
 ask :: String -> IO ()
 ask prompt =
+ do
+ putStrLn prompt
+ line <- getLine
+ if line == ""
+    then ask (prompt ++ "!") --append ! to prompt each time empty string entered
+    else if (line == "quit") then putStrLn("quitting...") --condition added for quitting
+    else do
+       putStrLn ("you said: " ++ reverse line)
+       ask prompt --needed for repeated inputs
+ 
+main :: IO ()
+main =
+ do
+ let prompt = "please say something"
+ ask prompt 
+
+  -- Echoes back input in reverse
+
+{--MODEL ANSWER
+ask :: String -> IO ()
+ask prompt =
   do
   putStrLn prompt
   line <- getLine
   if line == ""
     then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
-
+    else if (line == "quit") then putStrLn("quitting..") --condition added for quitting
+    else do
+      putStrLn ("you said: " ++ reverse line)
+      ask prompt --needed for repeated inputs
+        
 main :: IO ()
 main =
   do
   let prompt = "please say something"
-  ask prompt
+  ask prompt-}
